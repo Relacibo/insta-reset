af02ddf3e8335e43e85d42b93699dec6af4f580d
Reinhard Bronner <bronner_reinhard@t-online.de>
2021-10-20 22:46:49 +0200

    Button text of quit button changes, when pressing shift

--- src/main/java/de/rcbnetwork/insta_reset/mixin/client/GameMenuScreenMixin.java
+++ src/main/java/de/rcbnetwork/insta_reset/mixin/client/GameMenuScreenMixin.java
@@ -2,12 +2,16 @@ package de.rcbnetwork.insta_reset.mixin.client;
 
 import de.rcbnetwork.insta_reset.InstaReset;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,6 +34,18 @@ public class GameMenuScreenMixin extends Screen {
     @Unique
     Supplier<Iterator<String>> instaResetStatusTextSupplier = Collections::emptyIterator;
 
+    @Unique
+    Text QUIT_MESSAGE = new TranslatableText("menu.returnToMenu");
+
+    @Unique
+    Text RESET_MESSAGE = new LiteralText("Reset without saving");
+
+    @Unique
+    ButtonWidget QUIT_BUTTON;
+
+    @Unique
+    boolean shiftDownLast = false;
+
     @Inject(method = "<init>", at = @At("TAIL"))
     public void extendInit(CallbackInfo info) {
         if (!InstaReset.instance().isModRunning()) {
@@ -46,19 +62,28 @@ public class GameMenuScreenMixin extends Screen {
             this.textRenderer.draw(matrices, str, 0.0f, y, 0xffffff);
             yAtom.set(y + 10);
         });
+
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void extendTick(CallbackInfo info) {
+        boolean shiftDown = hasShiftDown();
+        if (shiftDown ^ shiftDownLast) {
+            shiftDownLast = shiftDown;
+            QUIT_BUTTON.setMessage(shiftDown ? RESET_MESSAGE : QUIT_MESSAGE);
+        }
     }
 
     @Redirect(method = "initWidgets", at = @At(value = "NEW", target = "net/minecraft/client/gui/widget/ButtonWidget", ordinal = 7))
     private ButtonWidget createExitButton(int x, int y, int width, int height, Text message, ButtonWidget.PressAction onPress) {
-        return new ButtonWidget(x, y, width, height, message, (b) -> {
-            long windowHandle = MinecraftClient.getInstance().getWindow().getHandle();
-            // Is shift pressed
-            boolean shiftPressed = InputUtil.isKeyPressed(windowHandle, 340) || InputUtil.isKeyPressed(windowHandle, 344);
-            InstaReset.instance().setCurrentServerShouldFlush(shiftPressed);
+         QUIT_BUTTON = new ButtonWidget(x, y, width, height, message, (b) -> {
+            boolean shiftPressed = hasShiftDown();
+            InstaReset.instance().setCurrentServerShouldFlush(hasShiftDown());
             if (!shiftPressed) {
                 InstaReset.instance().stop();
             }
             onPress.onPress(b);
         });
+         return QUIT_BUTTON;
     }
 }
